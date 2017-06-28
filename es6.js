00292271c750369168b35466ecc50986df90ec73
Konstantin Gladkikh <birhoff@gmail.com>
2017-06-28 20:46:12 +0300

    feat(core): New support eslint version 4
    Add support eslint version 4 and above. Fix all eslint issues inside package

--- es6.js
+++ es6.js
@@ -4,6 +4,6 @@ const commonRules = require('./lib/common-rules');
 const es6Rules = require('./lib/es6-rules');
 
 module.exports = {
-    extends: 'pedant',
+    'extends': 'pedant',
     rules: Object.assign({}, commonRules, es6Rules)
 };
